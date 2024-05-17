3c1acdd3a5ff94a16954d9a2034eb5c677e86d4f
lost_nd_xxx <shimayoshiba@gmail.com>
2024-05-18 08:05:25 +0900

    :bug: コピーボタン用スクリプト入れ忘れ

--- ghost/master/help/src/script.js
+++ ghost/master/help/src/script.js
@@ -1,3 +1,32 @@
+(function (d) {
+	const pre = d.querySelectorAll('pre');
+	if (pre.length == 0) {
+		return;
+	}
+	for (let i = 0; i < pre.length; i++) {
+		const btn = d.createElement('button');
+		btn.className = 'copy-button';
+		btn.textContent = 'Copy';
+		pre[i].insertBefore(btn, pre[i].firstElementChild);
+		btn.addEventListener('click', copy_code, false);
+	}
+})(document);
+
+function copy_code() {
+	const code = this.nextElementSibling.innerText;
+	if (navigator.clipboard) {
+		navigator.clipboard.writeText(code).then(() => {
+			this.textContent = 'コピーしました！';
+			setTimeout(() => {
+				this.textContent = 'Copy';
+			}, 1000);
+		}).catch(e => {
+			alert('コピーできませんでした\nお手数ですが手動でコピーしてください\n\n' + e.message);
+		});
+	} else {
+		alert('このブラウザは Clipboard API 非対応です\nお手数ですが手動でコピーしてください');
+	}
+}
 
 window.addEventListener('scroll', () => {
 	const pageTop = document.getElementById('page_top');
