7791effb7ce5544a850d212d8f62c159f3292299
lost_nd_xxx <shimayoshiba@gmail.com>
2024-05-18 05:10:14 +0900

    :fire: 不要なスクリプトの消し忘れを削除

--- docs/src/script.js
+++ docs/src/script.js
@@ -1,15 +1,3 @@
-let lnp_ss = {
-	cnt: 0,
-	num: 5,
-	nme: "./src/screenshot_",
-	ext: "png",
-	id: "lnp_ss"
-};
-function changeImage(obj) {
-	obj["cnt"]++;
-	obj["cnt"] %= obj["num"];
-	document.getElementById(obj["id"]).src = obj["nme"] + obj["cnt"] + "." + obj["ext"];
-};
 
 window.addEventListener('scroll', () => {
 	const pageTop = document.getElementById('page_top');
